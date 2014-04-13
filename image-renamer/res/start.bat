baaf6bd6b4d6db9ae5ba432c35e0c1648c19eb49
amarinkovic <amarinkovic@s4hc.com>
2014-04-13 19:03:41 +0100

    Output redirection to log file added

--- image-renamer/res/start.bat
+++ image-renamer/res/start.bat
@@ -2,6 +2,6 @@
 @SET TARGET_FOLDER_PATH=d:/temp
 @SET NAME_PREFIX=Scotland
 
-@java -cp image-renamer.jar com.am.image.tools.Renamer %SOURCE_FOLDER_PATH% %TARGET_FOLDER_PATH% %NAME_PREFIX%
+@java -cp image-renamer.jar com.am.image.tools.Renamer %SOURCE_FOLDER_PATH% %TARGET_FOLDER_PATH% %NAME_PREFIX% > renamer.log
 
 @pause
