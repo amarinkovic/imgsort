e05726d7527894f4fb783e62ba207410cb48dc16
amarinkovic <alexandar.marinkovic@gmail.com>
2019-12-21 23:07:06 +0100

    update maven changes

--- build/start.bat
+++
@@ -1,7 +0,0 @@
-@SET SOURCE_FOLDER_PATH=d:/other/2014-03-01-Scotland
-@SET TARGET_FOLDER_PATH=d:/temp
-@SET NAME_PREFIX=Scotland
-
-@java -cp image-renamer.jar com.am.image.tools.Renamer %SOURCE_FOLDER_PATH% %TARGET_FOLDER_PATH% %NAME_PREFIX% > renamer.log
-
-@pause
